6b072d76e735c4fc4e6909835f5661a12437ff58
adilkhash <adil@playrix.com>
2019-06-03 11:17:34 +0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+install_requires = [
+    'django>=2.0',
+    'qiwi-payments==0.1',
+]
+
+setup(
+    name='django-qiwi-kassa',
+    version='0.1',
+    packages=['qiwi_kassa'],
+    url='https://github.com/adilkhash/django-qiwi-kassa',
+    license='MIT',
+    author='Adylzhan Khashtamov',
+    author_email='adil.khashtamov@gmail.com',
+    description='Django App for Qiwi Kassa',
+    install_requires=install_requires,
+)
